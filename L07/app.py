8f6843aef04621177f2c20c0221314ae0ba772d7
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-05-25 22:55:45 +0530

    added formatted strings

---
+++ L07/app.py
@@ -0,0 +1,15 @@
+# formatted strings
+
+# first='John'
+# last='Smith'
+# message = first + ' [' + last + '] is a coder'
+# msg = f' {first} [{last}] is a coder'
+# print(msg)
+# print(message)
+# course='Python for Beginners'
+# print(len(course))
+
+course = 'python for Beginners'
+print(course)
+print(course.upper())
+print(course.find('B'))
