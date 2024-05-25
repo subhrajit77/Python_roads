9d9ba4f01430681a82b42bd87d2e95b17c209f22
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-05-25 23:17:25 +0530

    operators

---
+++ L08/app.py
@@ -0,0 +1,15 @@
+print(10*3)
+print(10**3)
+x=10
+# x=x+3
+# print(x)
+# x += 3
+# print(x)
+# y = 10 + 3 * 2
+# print(y)
+# exponentiation 
+# multiplication/divison
+# addition/subtraction 
+z=2.9
+print(round(z))
+print(abs(-2.9))
