3dbb87ba2bab27ee92910ca017c129ca4da8c564
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-05-25 23:41:02 +0530

    added new files

---
+++ L10 Conditionals/app.py
@@ -0,0 +1,12 @@
+is_hot = True
+is_cold = True
+if is_hot:
+    print("It's a hot day :( ")
+    print("Drink plenty of water :P ")
+elif is_cold:
+    print("It's a cold day :( ")
+    print("Wear warm clothes :P ")
+
+else  :
+    print("It's a lovely day")
+    
