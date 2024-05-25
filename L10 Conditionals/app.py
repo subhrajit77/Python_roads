716c4407b42b92d5259cdc1331661ef06e665549
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-05-25 23:47:39 +0530

    added new files

--- L10 Conditionals/app.py
+++ L10 Conditionals/app.py
@@ -1,12 +1,22 @@
-is_hot = True
-is_cold = True
-if is_hot:
-    print("It's a hot day :( ")
-    print("Drink plenty of water :P ")
-elif is_cold:
-    print("It's a cold day :( ")
-    print("Wear warm clothes :P ")
+# is_hot = True
+# is_cold = True
+# if is_hot:
+#     print("It's a hot day :( ")
+#     print("Drink plenty of water :P ")
+# elif is_cold:
+#     print("It's a cold day :( ")
+#     print("Wear warm clothes :P ")
 
-else  :
-    print("It's a lovely day")
+# else  :
+#     print("It's a lovely day")
+
+has_high_income = True
+has_good_credit = False
+
+if has_high_income and has_good_credit :
+    print("It's a good credit")
+elif has_high_income or has_good_credit :
+    print("It's a good income")  
+else :
+    print("It's a bad credit")      
     
