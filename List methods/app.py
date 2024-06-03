ef70a88677d0f3781cca3e7b50ae0b26664f810b
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-06-03 21:12:03 +0530

    added list methods

---
+++ List methods/app.py
@@ -0,0 +1,12 @@
+numbers = [5, 2, 1, 7, 4]
+numbers.append(20) 
+# insert or append
+# append doesnot need index to insert but insert needs index
+print(numbers)
+numbers.insert(0, 10)
+print(numbers)
+numbers.remove(5)
+print(numbers)
+# clear to remove all numbers 
+# numbers.clear()
+# print(numbers)
