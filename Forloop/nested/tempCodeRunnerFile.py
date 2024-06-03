276bac572e1aafbd4407497f61cd1c7b2d1bb392
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-06-03 20:24:43 +0530

    added arrays or list

---
+++ Forloop/nested/tempCodeRunnerFile.py
@@ -0,0 +1,1 @@
+    print('x' * x_count)
---
+++ List/Array/app.py
@@ -0,0 +1,8 @@
+names=['Miguel','Bob', 'Mosh', 'James', 'John', 'Mary']
+print(names[1])
+print(names[-1]) 
+# negative for last element
+print(names[1:3])
+print(names)
+names[4] = 'Jon'
+print(names)
