e4e04e7f8cadb0f3e694b53fdec5c384996b4ce0
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-06-04 23:30:35 +0530

    modified

--- List methods/sorting.py
+++ List methods/sorting.py
@@ -2,3 +2,12 @@ numbers = [5, 12, 2,78, 10, 2]
 numbers.sort()
 print(numbers)
 # sort function helps to sort the list
+# reverse reverses the list
+
+numbers.reverse()
+print(numbers)
+
+numbers2 = numbers.copy()
+print(numbers2)
+print(numbers2)
+print(numbers2)
