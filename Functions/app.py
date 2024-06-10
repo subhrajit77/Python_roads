8e4fc987db8b00cc9fd729840061550930d4f0e2
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-06-10 22:30:36 +0530

    updated functions

--- Functions/app.py
+++ Functions/app.py
@@ -1,6 +1,14 @@
-def greet_user():
-    print("Hello")
-    print("Welcome onboard!!")
+# def greet_user():
+#     print("Hello")
+#     print("Welcome onboard!!")
+# print("Start")
+# greet_user()
+# print("Ends")
+
+def greet_user(firstname, lastname):
+    print(f'Welcome {firstname} {lastname}!')
+    print("How are you doing?")
 print("Start")
-greet_user()
-print("Ends")
+greet_user("Subhrajit", "Talukdar")    
+greet_user("Raj","Chouhan")    
+greet_user("Akash", "Sharma")    
