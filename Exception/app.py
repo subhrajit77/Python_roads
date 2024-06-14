13ee03e78c840488d2ffa4a31d5ba1940bddcdeb
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-06-14 13:08:59 +0530

    updated

--- Exception/app.py
+++ Exception/app.py
@@ -1,5 +1,15 @@
+# try:
+#     age=int(input('Enter your age: '))
+#     print(age)
+# except ValueError:
+#     print('Invalid input')    
+
 try:
-    age=int(input('Enter your age: '))
-    print(age)
+    age = int(input('Enter your age: '))
+    income = 2000
+    risk = income / age
+    print(risk)
+except ZeroDivisionError:
+    print('Age cannot be zero')
 except ValueError:
-    print('Invalid input')    
+    print('Invalid input')
