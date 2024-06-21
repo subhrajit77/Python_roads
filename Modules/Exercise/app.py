275f6ed72f937da20b833c0274797f9ff8e28d84
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-06-21 17:30:19 +0530

    added exercise and modules

---
+++ Modules/Exercise/app.py
@@ -0,0 +1,4 @@
+import utils 
+from utils import find_max
+
+find_max([10, 20, 30, 40, 50])
---
+++ Modules/Exercise/utils.py
@@ -0,0 +1,8 @@
+numbers=[]
+def find_max(numbers):
+    max= numbers[0]
+    for number in numbers:
+        if number> max:
+            max=number
+    print(max)        
+
