51dfd2882c82208f94167c61ebb2858717f8aee3
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-06-22 11:55:27 +0530

    added packages

---
+++ packages/app.py
@@ -0,0 +1,7 @@
+# import ecommerce.shipping
+# ecommerce.shipping.calc_shipping()
+#  or
+from ecommerce.shipping import calc_shipping,calc_tax
+
+calc_shipping()
+calc_tax()
---
+++ packages/ecommerce/shipping.py
@@ -0,0 +1,5 @@
+def calc_shipping():
+    print("This is a Shipping method")
+
+def calc_tax():
+    print("This is a Tax method")
