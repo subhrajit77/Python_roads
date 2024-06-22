fe64ff3a6351b28c687261eaf812e50188943eab
Subhrajit Talukdar <117500946+subhrajit77@users.noreply.github.com>
2024-06-22 13:32:38 +0530

    added rolling dice

---
+++ random values/exercise.py
@@ -0,0 +1,11 @@
+import random 
+# rolling a dice
+
+class Dice:
+    def roll(self):
+        first = random.randint(1,6)
+        second = random.randint(1,6)
+        return first, second    
+
+dice = Dice()
+print(dice.roll())
